1316cb7a5968bf8b25cfd3568ade1487a5bfc138
Andy Keep <akeep@fb.com>
2022-08-27 16:36:31 -0700

    Added keymaps, options, and plugins.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,3 @@
--- indentation options
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.smartindent = true
-
--- syntax options
-vim.opt.showmatch = true
-
--- wrapping options
-vim.opt.wrap = false
-
--- wildmode options
-vim.opt.wildmode = { "list:longest", "full" }
-
--- mouse option
-vim.opt.mouse = "nv"
-
--- clipboard options
-vim.opt.clipboard:append "unnamedplus"
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
---
+++ .config/nvim/lua/user/plugins.lua
@@ -0,0 +1,32 @@
+-- start-up packer with a little protection to avoid raising error on failures.
+local status_ok, packer = pcall(require, "packer_boot")
+if not status_ok then
+  print("Packer load failed!")
+  return
+end
+
+-- initialize packer to use a floating window.
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  }
+}
+
+return require('packer_boot').startup(function(use)
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+
+  -- colorschemes
+  use "cpea2506/one_monokai.nvim"
+
+  -- Source control plugins.
+  use "ludovicchabant/vim-lawrencium"
+  use "tpope/vim-fugitive"
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
